52546e7d0c5298515b90b30be51142f1027cd383
Markus Falk <mail@markus-falk.com>
2022-05-05 17:55:56 +0200

    feat: setup project

---
+++ testing-tutorial/apps/angular-testing-modules/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'testing-tutorial-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-testing-modules';
+}
